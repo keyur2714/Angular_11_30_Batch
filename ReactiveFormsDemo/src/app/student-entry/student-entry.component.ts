b118ee28ac327e6e7ddb2aae8d101f6d0f7c0d78
keyur2714 <keyur.555kn@gmail.com>
2018-06-02 10:49:48 +0530

    updated reactive forms demo

--- ReactiveFormsDemo/src/app/student-entry/student-entry.component.ts
+++ ReactiveFormsDemo/src/app/student-entry/student-entry.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl,Validators, FormArray } from '@angular/forms';
+import { FormGroup,FormControl,Validators, FormArray, FormBuilder } from '@angular/forms';
 import { Student } from './student-model';
 @Component({
   selector: 'app-student-entry',
@@ -10,18 +10,20 @@ export class StudentEntryComponent implements OnInit {
 
   student : Student = new Student();
   isSubmitted = false;
-
+  namePattern = "^[a-z0-9_-]{8,15}$";
   studentDetailForm = new FormGroup(
     {
-      name : new FormControl('keyur',[Validators.required,Validators.minLength(3)]),
-      age: new FormControl(30,Validators.required),
-      city: new FormControl('',Validators.required),
-      mobileNo: new FormControl('',Validators.required),
-      hobbies: new FormArray([])      
+      name : new FormControl('keyur',[Validators.required,Validators.minLength(3),Validators.pattern(this.namePattern)]),
+      age: new FormControl(30,Validators.required),      
+      address: this.fb.group({ // <-- the child FormGroup
+        city: new FormControl('',Validators.required),
+        mobileNo: new FormControl('',Validators.required)
+      }),
+      hobbies: new FormArray([])            
     }
   );
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
     (<FormArray>this.studentDetailForm.get('hobbies')).push(new FormControl());    
@@ -29,8 +31,9 @@ export class StudentEntryComponent implements OnInit {
 
   save(){
     console.log(this.studentDetailForm);
+    console.log(this.studentDetailForm.controls["address"].get("city").value);
     console.log(this.studentDetailForm.value);
-    console.log(this.student.name+" "+this.student.city+" "+this.student.age+" "+this.student.mobileNo+" "+this.student.hobby);
+    console.log(this.student.name+" "+this.student.city+" "+this.student.age+" "+this.student.mobileNo+" "+this.student.hobbies);
     this.isSubmitted = true;
   }
 
